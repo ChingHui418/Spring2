bd776f23389c8f809b5a774e137241ccfedf0767
ChingHui418 <a0985218998@gmail.com>
2026-02-10 09:06:09 +0800

    20260210

---
+++ src/test/java/tw/hui/spring2/Spring02ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.hui.spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
